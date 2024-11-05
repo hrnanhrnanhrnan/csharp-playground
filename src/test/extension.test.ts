22ae7153d6fe13486019bfc1d8e7f9effd3caec3
Mike Jones <kiamel76@gmail.com>
2024-11-05 19:28:18 +0100

    Testing push to dev

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,5 +1,7 @@
 import * as assert from 'assert';
 
+// MAJK WAS HERE IN DEV ALSO!
+
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
