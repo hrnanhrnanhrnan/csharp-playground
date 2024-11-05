7fe95c808345dba88b358d22557661cda334d49e
Mike Jones <kiamel76@gmail.com>
2024-11-05 19:36:02 +0100

    Push majks feature branch to be PR

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,5 +1,7 @@
 import * as assert from 'assert';
 
+// MAJKS EGEN FEATURE BRANCH!
+
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
