c6e9a9996c2f86f7956230239d0d0a2111d0ed37
Mike Jones <kiamel76@gmail.com>
2024-11-07 16:36:36 +0100

    Commit for PR testing thursday

--- src/test/extension.test.ts
+++ src/test/extension.test.ts
@@ -1,5 +1,7 @@
 import * as assert from 'assert';
 
+// W3lcome to Sogeti!
+
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
 import * as vscode from 'vscode';
