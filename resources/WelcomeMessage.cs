9e6a93e13cbea93c3df167114037016e9c252524
Robin <hrnan@live.se>
2024-11-07 13:48:40 +0100

    updated welcome message and server retries

--- resources/WelcomeMessage.cs
+++ resources/WelcomeMessage.cs
@@ -1,5 +1,14 @@
-// Thank you for using "csharp-playground"
-// Make sure to have the Editor > Inlay Hints: Enabled set to "true" in your settings 
-// to get inlay values of your global variables and stuff written to the console
+/* 
+Thank you for using "C# Playground"!
+Make sure to have the "Editor > Inlay Hints: Enabled" (editor.inlayHints.enabled) 
+property set to "on" in your settings to get inline values 
+of your global variables and console outputs
+
+NOTE: 
+- Only global variables, i.e. variables declared at the top level, 
+    will have their values displayed inline
+- All code has to be placed here in Program.cs, 
+    types can NOT be declared in separate files 
+*/
 
 Console.WriteLine("Hello from Playground!");
